963d3e29a497345c5d506a4a957f40428f2961ae
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-06-29 18:26:25 +0300

    Initial import
    Add Polygon* classes, pack_header, write_polygons, __main__

---
+++ write_polygons.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+from typing import Iterator, Self, BinaryIO, cast
+import os
+from itertools import chain
+import struct
+import header as H
+
+
+class Polygon:
+    def __init__(self, bytesdata: bytes):
+        self.view = memoryview(bytesdata)
+
+    # @classmethod
+    # def from_file(cls, f: BinaryIO, fmt: str) -> Self:
+    #     (sz,) = struct.unpack("<i", f.read(4))
+    #     return cls(f.read(sz), fmt)
+
+
+class PolygonStr(Polygon):
+    def __init__(self, bytesdata, fmt):
+        super().__init__(bytesdata)
+        self.fmt = fmt
+
+    @classmethod
+    def from_file(cls, f: BinaryIO, fmt: str) -> Self:
+        (sz,) = struct.unpack("<i", f.read(4))
+        return cls(f.read(sz), fmt)
+
+    def __iter__(self) -> Iterator[tuple[float, float]]:
+        sz = struct.calcsize(self.fmt)
+        for off in range(0, len(self.view), sz):
+            s = slice(off, off + sz)
+            yield struct.unpack_from(self.fmt, self.view[s])
+
+
+class PolygonType(Polygon):
+    def __init__(self, bytesdata: bytes, factory: H.ViewMeta):
+        super().__init__(bytesdata)
+        self.factory = factory
+
+    @classmethod
+    def from_file(cls, f: BinaryIO, factory: H.ViewMeta) -> Self:
+        (sz,) = struct.unpack("<i", f.read(4))
+        return cls(f.read(sz), factory)
+
+    def __iter__(self) -> Iterator[H.ViewMeta]:
+        sz = self.factory.data_size
+        for off in range(0, len(self.view), sz):
+            s = slice(off, off + sz)
+            yield self.factory(self.view[s])
+
+
+def pack_header(polygons: H.PolygonType = H.polygons) -> bytes:
+    magic = 0x1234
+    x1 = min(x for x, _ in chain(*polygons))
+    y1 = min(y for _, y in chain(*polygons))
+    x2 = min(x for x, _ in chain(*polygons))
+    y2 = min(y for _, y in chain(*polygons))
+    num_points = len(polygons)
+    return struct.pack("<iddddi", magic, x1, y1, x2, y2, num_points)
+
+
+def write_polygons(f: BinaryIO, polygons: H.PolygonType = H.polygons) -> None:
+    f.write(pack_header())
+    for poly in polygons:
+        fmt: str = "<dd"
+        sz = struct.calcsize(fmt)
+        f.write(struct.pack("<i", sz * len(poly)))
+        for pp in poly:
+            f.write(struct.pack(fmt, *pp))
+
+
+if __name__ == "__main__":
+    # if not os.path.exists("polygons.bin"):
+    #     with open("polygons.bin", "wb") as f:
+    #         write_polygons(f)
+    #         print("polygons.bin was written")
+    with open("polygons.bin", "rb") as f:
+        h: H.Header = H.Header(f.read(H.Header.data_size))
+        for _ in range(cast(int, h.num_polygons)):
+            # polygon = PolygonStr.from_file(f, "<dd")
+            # for pp in polygon:
+            #     print(pp)
+            polygon = PolygonType.from_file(f, H.Point)
+            points: list[H.Point] = []
+            pp: H.Point
+            for pp in polygon:
+                # print(pp)
+                points.append(pp)
+            print(points)
